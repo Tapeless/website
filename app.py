5c22bb00b864a6472f8253c00487468dfdabaa69
Brendon Kirouac <tap3less@gmail.com>
2024-02-03 17:39:31 -0500

    Switch to flask

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from flask import render_template
+from flask import send_from_directory
+
+app = Flask(__name__)
+
+#@app.route('/favicon.ico')
+#def favicon():
+    #return send_from_directory(os.path.join(app.root_path, 'static'),
+                               #'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
